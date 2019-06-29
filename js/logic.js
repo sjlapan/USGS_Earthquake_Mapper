662524360e1a921d7314c72e53f12dabe7eddc54
sjlapan <stewartjlapan@gmail.com>
2019-06-28 18:40:22 -0600

    Colored circles by magnitude and added tooltips. Working on adding a legend

--- js/logic.js
+++ js/logic.js
@@ -15,51 +15,66 @@ var last_week_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/a
 d3.json(last_week_url, function (response) {
 
     console.log(response.features);
-    
+
     // The following function is only returning the default. 
     // Unsure if that's because the magnitudes in the features are all too small
     // or if it isn't working properly. Find a way to scale the color classes to
     // the existing magnitude range in the data...
     // Possibly replace witha chloropleth layer?
+
+    var magVals = response.features.map(f => f.properties.mag)
+    // var magMin = Math.min(magVals)
+    var magMin = Math.min.apply(Math, magVals)
+    var magMax = Math.max.apply(Math, magVals)
+    console.log(magVals)
+    console.log(magMax)
     function magColor(mag) {
-        switch (mag) {
-            case (mag > 6.66):
-            return "red";
-            case (mag > 3.33):
-                return "orange";
-            default:
-                return "yellow";
+            return mag > (0.667 * magMax) ? "red" :
+            mag > (0.333 * magMax) ? "orange" :
+                "yellow"
         }
-    }
-// Test choropleth version
-    var earthquakes = L.choropleth(response.features, {
-        valueProperty: properties.mag
-    })
 
 
 // Test geoJSON version
     var earthquakes = L.geoJSON(response.features, {
-        // onEachFeature: function (feature, layer) {
-        
-        style: function(feature) {
-                // Need to write a function for specifying color
-                return {color: magColor(feature.properties.mag),
-                }
-            },
-            pointToLayer: function(feature, latlng){
-                return new L.CircleMarker(latlng, {
-                    radius: feature.properties.mag * 4,
-                    fillOpacity: 0.5 
-                });
-            },
-            // var datePart = new Date(feature.properties.time)
-            // layer.bindPopup()
-        // }
+    // onEachFeature: function (feature, layer) {
 
-});
+    style: function (feature) {
+        // Need to write a function for specifying color
+        return {
+            color: magColor(feature.properties.mag),
+        }
+    },
+    pointToLayer: function (feature, latlng) {
+        return new L.CircleMarker(latlng, {
+            radius: feature.properties.mag * 4,
+            fillOpacity: 0.5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        var datePart = new Date(feature.properties.time)
+        layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3>
+            <div>${datePart}</div>`)
+    }
+})
 
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2psYXBhbiIsImEiOiJjangxMzFyYWIwNGdzNDhsMzQ4Ymc5ZDVrIn0.oooU0Tjw_Wdamm6j95Ofwg",
-        { attribution: "Leaflet" });
+
+var legend = L.control({position: "bottomright"});
+legend.onAdd = function() {
+    var legendInfo = `<h1>Earthquake Magnitude</h1>
+    <div>
+      <div class="min">${magMin}</div>
+      <div class="max">${magMax}</div>
+    </div>
+  `;
+}
+
+
+
+
+
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2psYXBhbiIsImEiOiJjangxMzFyYWIwNGdzNDhsMzQ4Ymc5ZDVrIn0.oooU0Tjw_Wdamm6j95Ofwg",
+    { attribution: "Leaflet" });
 
 
 var baseMap = {
@@ -68,7 +83,7 @@ var baseMap = {
 
 var overlayMaps = {
     Earthquakes: earthquakes
-    };
+};
 
 var myMap = L.map("map", {
     center: [
@@ -77,8 +92,8 @@ var myMap = L.map("map", {
     zoom: 4,
     layers: [streetmap, earthquakes]
 })
+});
 
-})
 //     var quake_layer = L.geoJSON().addTo(map);
 //     quake_layer.addData(response); 
 // }
